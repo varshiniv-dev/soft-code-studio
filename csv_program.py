620ea0f6422885d3ca69eb0ba68ba949c7b49bd9
varshiniv-dev <varshiniv.dev@gmail.com>
2025-11-29 21:36:54 +0530

    Replaced old CSV files with my new program and dataset

--- csv_program.py
+++ csv_program.py
@@ -1,12 +1,18 @@
 # write a program using csv.writer for writing csv files in python
 import csv
-with open('data.csv', mode='w', newline='') as file:
+with open('hollywood.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Name', 'Age', 'City'])
-    writer.writerow(['Gojo', 30, 'Tokyo'])
-    writer.writerow(['Sinchan', 5, 'Kasukabi'])
-    writer.writerow(['Naruto', 50, 'Hidden life in village'])
-    writer.writerow(['luffy', 19, 'goa kindom'])
+    writer.writerow(['Name', 'Character_name', 'Character_age', 'Series'])
+    writer.writerow(['Ian Somerhalder ',  'Damon Salvatore ',
+                    41, ' The Vampire Diaries'])
+    writer.writerow(['Noah Schnapp ',  'Will Byers ', 16, ' Stranger Things'])
+    writer.writerow(['Daniel Radcliffe ', 'Harry potter ',
+                    17, ' Harry potter series'])
+    writer.writerow(['Park Solomon ', 'Lee Suhyeok ',
+                    17, ' All of us are dead'])
+    writer.writerow(['Song Hye-kyo ', 'Moon Dong-Eun ', 34, ' The Glory'])
+    writer.writerow(['Wo Do-hwan ', 'Hae Jo ', 30, ' Mr. Plankton'])
+
 # write a program using dictornary writer to as a dictonary for writing csv files in python
 with open('data_dict.csv', mode='w', newline='') as file:
     fieldnames = ['Name', 'Age', 'City']
