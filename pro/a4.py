8dd5eb33d5c94064ccd87ff98a0ce95f35d761e4
varshiniv-dev <varshiniv.dev@gmail.com>
2026-09-25 19:26:49 +0530

    added..

---
+++ pro/a4.py
@@ -0,0 +1,10 @@
+import operations as op
+
+a = 10
+b = 5
+
+print("Addition:", op.add(a, b))
+print("Subtraction:", op.subtract(a, b))
+print("Multiplication:", op.multiply(a, b))
+print("Division:", op.divide(a, b))
+print("Power:", op.power(a, b))
